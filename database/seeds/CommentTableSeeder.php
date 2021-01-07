f3081994c8bad75a0f06d7f11af92c93e29b6a6e
amr <amr_dotnet@ymail.com>
2021-01-07 07:00:07 +0200

    enhancement seeding

--- database/seeds/CommentTableSeeder.php
+++ database/seeds/CommentTableSeeder.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Models\Comment;
+use Carbon\Carbon;
+use Faker\Factory;
 use App\Models\Post;
 use App\Models\User;
-use Faker\Factory;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class CommentTableSeeder extends Seeder
@@ -16,21 +17,35 @@ class CommentTableSeeder extends Seeder
     public function run()
     {
         $faker = Factory::create();
-
+        $comments    = [];
         $users = collect(User::where('id', '>', 2)->get()->modelKeys());
         $posts = collect(Post::wherePostType('post')->whereStatus(1)->whereCommentAble(1)->get());
 
-        for ($i = 0; $i < 100; $i++) {
-            Comment::create([
-                'name'      => $faker->name,
-                'email'     => $faker->email,
-                'url'       => $faker->url,
+        for($i = 0 ; $i < 5000; $i++) {
+
+            $selected_post = $posts->random();
+            $post_date = $selected_post->created_at->timestamp;
+            $current_date = Carbon::now()->timestamp;
+
+            $comments[] = [
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'url' => $faker->url,
                 'ip_address' => $faker->ipv4,
-                'comment'   => $faker->paragraph(2, true),
-                'status'    => rand(0, 1),
-                'post_id'   => $posts->random()->id,
-                'user_id'   => $users->random()
-            ]);
+                'comment' => $faker->paragraph(2, true),
+                'status' => rand(0, 1),
+                'post_id' => $selected_post->id,
+                'user_id' => $users->random(),
+                'created_at' => date('Y-m-d H:i:s', rand($post_date, $current_date)),
+                'updated_at' => date('Y-m-d H:i:s', rand($post_date, $current_date)),
+            ];
+
+        }
+
+
+        $chunks = array_chunk($comments, 500);
+        foreach ($chunks as $chunk) {
+            Comment::insert($chunk);
         }
     }
 }
